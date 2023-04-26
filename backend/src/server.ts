96afb2aa1850e0cc277fc594817f0dc9acadb1f6
RevanToma <tamrevan@gmail.com>
2023-04-26 20:14:55 +0200

    fixed the server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,17 +1,13 @@
 import { connectToMongoDB } from "./db/mongoose_connection";
 import { app } from "./app";
 
-import { addUser, getUser, removeUser } from "./utils/addUser";
 const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
     await connectToMongoDB().then(() => console.log("Connected to DB..."));
-
-    // Add the following lines for Socket.IO
-
-    server.listen(port, () => {
-      console.log(`listening to ${port}:  http://127.0.0.1:${port}`);
+    app.listen(port, () => {
+      console.log(`listening to ${port}:  http://127.0.0.1:8000`);
     });
   } catch (error: any) {
     console.error(error.message);
