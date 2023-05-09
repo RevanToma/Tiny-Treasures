e1acc25bb80c37b2e8581050b35407964f2e57b9
JoarTaylor <joar.taylor@outlook.com>
2023-05-09 11:33:25 +0200

    edit

--- web-app/src/api/index.ts
+++ web-app/src/api/index.ts
@@ -4,10 +4,21 @@ const api = axios.create({
   baseURL: "http://127.0.0.1:8000/api/v1",
 });
 
-// api.interceptors.request.use(async (config) => {
-//   config.headers["Content-Type"] = "application/json";
+function getCookie(n: string) {
+  const a = `; ${document.cookie}`.match(`;\\s*${n}=([^;]+)`);
+  return a ? a[1] : "";
+}
 
-//   return config;
-// });
+api.interceptors.request.use(async (config) => {
+  config.headers["Content-Type"] = "application/json";
+
+  const cookie = getCookie("jwt");
+
+  if (cookie) {
+    config.headers["Authorization"] = `Bearer ${cookie}`;
+  }
+
+  return config;
+});
 
 export default api;
