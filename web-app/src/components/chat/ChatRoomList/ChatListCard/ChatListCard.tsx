c5abd10b1c329e5572ad6184d35f1450a3b67101
JoarTaylor <joar.taylor@outlook.com>
2023-05-12 16:58:30 +0200

    edit

--- web-app/src/components/chat/ChatRoomList/ChatListCard/ChatListCard.tsx
+++ web-app/src/components/chat/ChatRoomList/ChatListCard/ChatListCard.tsx
@@ -4,7 +4,10 @@ import { IChatRoom } from "../../../../types";
 import { useSelector } from "react-redux";
 import { selectUser } from "../../../../store/user/userSelectors";
 import * as S from "./styled";
-import { getHoursAndMinutes } from "../../../../utils/helperfunctions";
+import {
+  getFullDate,
+  getHoursAndMinutes,
+} from "../../../../utils/helperfunctions";
 
 type ChatListCardProps = {
   room: IChatRoom;
@@ -25,6 +28,9 @@ const ChatListCard: React.FC<ChatListCardProps> = ({
   const lastMessageDate = room.messages[room.messages.length - 1]?.createdAt;
   if (!lastMessageDate) return null;
   const dateString = Date.parse(lastMessageDate.toString());
+  const messageDay = new Date(dateString).getDay();
+  const todaysDay = new Date().getDay();
+  const isToday = messageDay === todaysDay;
 
   return (
     <S.Card key={room._id} onClick={() => handleSwitchChat(room)}>
@@ -34,7 +40,11 @@ const ChatListCard: React.FC<ChatListCardProps> = ({
         <h2>{lastSenderNotMeId ? "other" : userName}</h2>
         <Box width="100%" flexDirection="row" justifyContent="space-between">
           <h3>{lastMessage}</h3>
-          <h3>{getHoursAndMinutes(dateString)}</h3>
+          <h5>
+            {isToday
+              ? getHoursAndMinutes(dateString)
+              : `${getFullDate(dateString)} ${getHoursAndMinutes(dateString)}`}
+          </h5>
         </Box>
       </Box>
     </S.Card>
