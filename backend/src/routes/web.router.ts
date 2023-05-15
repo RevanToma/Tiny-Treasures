ac4c1d95c483fdceaf9a9b522f39ae8991569bad
RevanToma <tamrevan@gmail.com>
2023-05-15 12:00:25 +0200

    google stuf

--- backend/src/routes/web.router.ts
+++ backend/src/routes/web.router.ts
@@ -1,24 +1,26 @@
-import express from 'express';
-import passport from 'passport';
-import * as authController from '../controllers/authController';
-import { CustomRequest } from '../utils/expressInterfaces';
+import express from "express";
+import passport from "passport";
+import * as authController from "../controllers/authController";
+import { CustomRequest } from "../utils/expressInterfaces";
 
 const router = express.Router();
 
 router.get(
-  '/auth/google',
-  passport.authenticate('google', { scope: ['email', 'profile'] })
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
 );
 
 router.get(
-  '/auth/google/callback',
-  passport.authenticate('google', { session: false }),
+  "/auth/google/callback",
+  passport.authenticate("google", { session: false }),
   authController.googleAuthCallback
 );
 
-router.get('/profile', authController.protect, (req, res) => {
-  console.log('success');
+router.get("/", authController.protect, (req, res) => {
+  console.log("success");
   res.send(`logged in! ${req.user}`);
 });
+
+router.get("/getUser", authController.protect, authController.sendUser);
 
 export default router;
