31b19c5295d0dacc4cad142588274298a3621af8
JoarTaylor <joar.taylor@outlook.com>
2023-05-22 12:41:55 +0200

    remove es-lint errors

--- web-app/src/main.tsx
+++ web-app/src/main.tsx
@@ -5,9 +5,6 @@ import { Provider } from "react-redux";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import store from "./store/store.ts";
-import { PersistGate } from "redux-persist/integration/react";
-import { persistStore } from "redux-persist";
-import Spinner from "./components/common/spinner/spinner.component.tsx";
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -15,8 +12,6 @@ export const queryClient = new QueryClient({
     },
   },
 });
-
-const persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
